b98b498dccf46704c203b93bb9de6e789da19d83
Roberto Monzón <rm15830921@outlook.com>
2026-05-03 21:29:27 -0600

    feat: add copy functionality, particles background, and JS-based drag prevention

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const text = "Desarrollador Web Junior";
 const animation = document.getElementById("profession");
-const toast = document.getElementById("toast");
 let i = 0;
 
+const toast = document.getElementById("toast");
+const images = document.querySelectorAll("img");
+const copyEls = document.querySelectorAll(".copy");
+
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+const particleColor = "#ffffffdb";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particles = [];
+
+const isMobile = window.innerWidth < 768;
+const PARTICLE_COUNT = isMobile ? 35 : 80;
+const speed = isMobile ? 0.5 : 1.2;
+
+/* PROFESSION TYPE EFFECT */
 function typeEffect() {
 
     if (i < text.length) {
@@ -13,13 +28,22 @@ function typeEffect() {
 
 }
 
-document.querySelectorAll(".copy").forEach(el => {
+/* DRAG KILLER */
+images.forEach(img => {
+
+    img.addEventListener("dragstart", e => e.preventDefault());
+    img.setAttribute("draggable", "false");
+
+});
+
+/* COPY FUNCTION */
+copyEls.forEach(el => {
 
     el.addEventListener("click", () => {
 
         navigator.clipboard.writeText(el.dataset.copy);
 
-        el.style.color = "rgb(0, 255, 150)";
+        el.style.color = "#00e1ff";
 
         showToast("Copiado al Portapapeles ✔️");
 
@@ -33,6 +57,7 @@ document.querySelectorAll(".copy").forEach(el => {
 
 });
 
+/* COPY TOAST */
 function showToast(message) {
 
     toast.textContent = message;
@@ -43,5 +68,48 @@ function showToast(message) {
     }, 1200);
 
 }
+
+/* RANDOM SPAWN */
+for (let c = 0; c < PARTICLE_COUNT; c++) {
+
+    particles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        vx: (Math.random() - 0.5) * speed,
+        vy: (Math.random() - 0.5) * speed,
+        size: Math.random() * 3 + 0.5
+    });
+}
+
+/* ANIMATION FUNCTION */
+function animate() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = particleColor;
+
+    particles.forEach(p => {
+        p.x += p.vx;
+        p.y += p.vy;
+
+        if (p.x < 0 || p.x > canvas.width) p.vx *= -1;
+        if (p.y < 0 || p.y > canvas.height) p.vy *= -1;
+
+        ctx.beginPath();
+        ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
+        ctx.fill();
+    });
+
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+/* CANVAS SIZE */
+window.addEventListener("resize", () => {
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+});
 
 typeEffect();
